8200086153c0d203c4a5345adfd666489718eca7
Alexander Diaz <alexgdiaz98@gmail.com>
2018-11-05 22:05:11 -0500

    Initial Implementation

--- src/main/java/dataanalysis/Analyzer.java
+++ src/main/java/dataanalysis/Analyzer.java
@@ -1,5 +1,23 @@
 package dataanalysis;
 
+import corelogic.Route;
+import corelogic.Stop;
+
 public class Analyzer {
+
+    public Analyzer() {
+    }
+
+    public int addStop(Stop stop) {
+        return stop.passengerQueue.size();
+    }
+
+    public int addRoute(Route route) {
+        int sum = 0;
+        for (int i = 0; i < route.stops.size(); i++) {
+            sum += route.stops.get(i).passengerQueue.size();
+        }
+        return sum/route.stops.size();
+    }
 
 }
