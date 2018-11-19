11ae97539e7dad3f076bf5a7626a1fd7f44bad84
Alexander Diaz <alexgdiaz98@gmail.com>
2018-11-18 22:27:45 -0500

    Adjusted Interfacer constructor for testing and added new tests

--- src/test/java/dataanalysis/InterfacerTest.java
+++ src/test/java/dataanalysis/InterfacerTest.java
@@ -28,24 +28,61 @@ public class InterfacerTest {
         stops.add(new Stop(1, "Tech Square", 3.1, 0.4, 2.03));
         stops.add(new Stop(2, "IC", 1.2, 0.89, 0.89));
         stops.add(new Stop(3, "Van Leer", 1.2, 0.89, 0.89));
+        stops.add(new Stop(4, "Biotech Quad", 1.2, 0.89, 0.89));
+        stops.add(new Stop(5, "Student Center", 1.2, 0.89, 0.89));
 
-        Route r1 = new Route(0, stops, false);
+        Route r1 = new Route(0, stops.subList(0, 4), false);
+        Route r2 = new Route(1, stops.subList(2, 6), true);
         routes.add(r1);
-        Interfacer testInterfacer = new Interfacer(buses, stops, routes);
+        routes.add(r2);
+        Interfacer testInterfacer = new Interfacer(buses, stops, routes,
+                "full.DOT");
         testInterfacer.createGraph();
 
     }
 
+    @Test
+    public void cycleWriterTest() {
+        stops.add(new Stop(0, "CULC", 0.1, 2, 0.45));
+        stops.add(new Stop(1, "Tech Square", 3.1, 0.4, 2.03));
+        stops.add(new Stop(2, "IC", 1.2, 0.89, 0.89));
+        stops.add(new Stop(3, "Van Leer", 1.2, 0.89, 0.89));
+
+        Route r1 = new Route(0, stops.subList(0, 4), false);
+        routes.add(r1);
+        Interfacer testInterfacer = new Interfacer(buses, stops, routes,
+                "cycle.DOT");
+        testInterfacer.createGraph();
+    }
+
+    @Test
+    public void dagWriterTest() {
+        stops.add(new Stop(2, "IC", 1.2, 0.89, 0.89));
+        stops.add(new Stop(3, "Van Leer", 1.2, 0.89, 0.89));
+        stops.add(new Stop(4, "Biotech Quad", 1.2, 0.89, 0.89));
+        stops.add(new Stop(5, "Student Center", 1.2, 0.89, 0.89));
+
+        Route r1 = new Route(0, stops.subList(0, 4), true);
+        routes.add(r1);
+        Interfacer testInterfacer = new Interfacer(buses, stops, routes,
+                "dag.DOT");
+        testInterfacer.createGraph();
+
+    }
+
+
     @Test(expected = IllegalArgumentException.class)
     public void constructorNullExceptionTest() {
-        Interfacer testInterfacer = new Interfacer(buses, null, routes);
+        Interfacer testInterfacer = new Interfacer(buses, null, routes,
+                "simulation.DOT");
 
     }
 
     @Test
     public void constructorTest() {
         stops.add(new Stop(1, "Harold Dr. and 5th", 0.4, 7.0, 1.0));
-        Interfacer testInterfacer = new Interfacer(buses, stops, routes);
+        Interfacer testInterfacer = new Interfacer(buses, stops, routes,
+                "simulation.DOT");
         assertEquals(testInterfacer.getStops().get(0).getName(), "Harold Dr. and 5th");
         assertEquals(testInterfacer.getStops().get(0).getX(), 0.4, 0.01);
         assertEquals(testInterfacer.getStops().get(0).getX(), 0.4, 0.01);
